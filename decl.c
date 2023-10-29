a334a97641068c607eef2aa9f8b43c1621e60b43
Junwen <jan2@nd.edu>
2023-10-29 17:17:13 -0400

    add param_list print, type, decl

--- decl.c
+++ decl.c
@@ -1,8 +1,8 @@
 #include "decl.h"
 
 struct decl * decl_create( char *name, struct type *type, struct expr *value, struct stmt *code, struct decl *next ){
-    struct decl *d = malloc(sizeof(struct decl));
-    d->name = strdup(name);
+    struct decl *d = malloc(sizeof(*d));
+    d->name = name;
     d->type = type;
     d->value = value;
     d->code = code;
---
+++ param_list.c
@@ -0,0 +1,19 @@
+#include "param_list.h"
+
+struct param_list * param_list_create( char *name, struct type *type, struct param_list *next ){
+    struct param_list *p = malloc(sizeof(*p));
+    p->name = name;
+    p->type = type;
+    p->next = next;
+    return p;
+}
+
+void param_list_print( struct param_list *a ){
+    if(!a) return;
+    printf("%s: ", a->name);
+    while(a->next){
+        printf(", ");
+        a = a->next;
+        printf("%s: ", a->name);
+    }
+}
