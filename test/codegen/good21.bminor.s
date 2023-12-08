c0e7214f67d33c95c23da703843c9a1068979624
Junwen <jan2@nd.edu>
2023-12-07 22:09:57 -0500

    added test with escape characters in string

---
+++ test/codegen/good21.bminor.s
@@ -0,0 +1,35 @@
+.text
+.globl main
+main:
+	PUSHQ %rbp
+	MOVQ %rsp, %rbp
+	SUBQ $40, %rsp
+	PUSHQ %rbx
+	PUSHQ %r12
+	PUSHQ %r13
+	PUSHQ %r14
+	PUSHQ %r15
+.data
+.L0:
+	.string "Hello World\nHello\tWorld"
+.text
+	LEAQ .L0, %rbx
+	MOVQ %rbx, %rdi
+	PUSHQ %r10
+	PUSHQ %r11
+	CALL print_string
+	POPQ %r11
+	POPQ %r10
+	MOVQ %rax, %rbx
+	MOVQ $0, %rbx
+	MOV %rbx, %rax
+	JMP .main_epilogue
+.main_epilogue:
+	POPQ %r15
+	POPQ %r14
+	POPQ %r13
+	POPQ %r12
+	POPQ %rbx
+	MOVQ %rbp, %rsp
+	POPQ %rbp
+	RET
